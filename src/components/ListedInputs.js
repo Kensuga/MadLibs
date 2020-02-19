8744480506ca5db3448a07ce40baf6257493cfb0
learn <learn@LEARN-12.local>
2020-02-18 16:47:59 -0800

    Hard Coded Version

--- src/components/ListedInputs.js
+++ src/components/ListedInputs.js
@@ -1,36 +1,149 @@
 import React, { Component } from 'react';
-import Input from './Input'
+import Paragraph from './Paragraph';
 
-export default class ListedInputs extends Component {
-    constructor(){
-        super()
+class ListedInputs extends Component {
+    constructor(props){
+        super(props)
         this.state = {
-            inputArray: []
-            }
-        }
+            frName: "",
+            numHr: "",
+            vehicle: "",
+            adjective1: "",
+            adjective2: "",
+            ingVerb: "",
+            animal: "",
+            adjective3:"",
+            ptVerb: "",
+            adjective4: "",
+            noun: "",
+            ptVerb2:"",
+            ptVerb3:"",
+            place:"",
+            verb:"",
+            isTrue: false,
+            diabled: false
+        } 
+    }
 
-    addInput = (input) =>{
-        let tempArray = [] 
-        tempArray.push(this.props.input)
-        this.setState({ inputArray: tempArray })
-        console.log(this.state.inputArray);
+    updateName = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit = () => {
+        this.setState({ disabled: true });
+        this.setState({ isTrue: true });
+    }
+
+
+    clearParagraph = () => {
+        this.setState({ disabled: false });
+        this.setState({ isTrue: false });
+        window.location.reload();
     }
 
     render(){
         return (
             <div>
-                <Input 
-                    type={ "Friend's Name " }
-                    updateArray= { this.updateArray }
-                    addInput = { this.addInput }
-                />
-                <Input
-                    type ={ "Hours # " }
-                    updateArray = {this.updateArray }
-                    addInput = { this.addInput }
-                />
-                <button>Submit</button>
+                <form>
+                    <input
+                        onChange= { this.updateName }
+                        name = "frName"
+                        placeholder = "Friend's Name"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        onChange= { this.updateName }
+                        name = "numHr"
+                        placeholder= "Number of Hours"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="vehicle"
+                        onChange= { this.updateName }
+                        placeholder= "Vehicle"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        onChange= { this.updateName }
+                        name = "adjective1"
+                        placeholder= "Adjective"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="adjective2"
+                        onChange= { this.updateName }
+                        placeholder= "Adjective"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="ingVerb"
+                        onChange= { this.updateName }
+                        placeholder= "-ing Verb"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="animal"
+                        onChange= { this.updateName }
+                        placeholder= "Animal"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="adjective3"
+                        onChange= { this.updateName }
+                        placeholder= "Adjective"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="ptVerb"
+                        onChange= { this.updateName }
+                        placeholder= "Past Tense Verb"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="adjective4"
+                        onChange= { this.updateName }
+                        placeholder= "Adjective"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="noun"
+                        onChange= { this.updateName }
+                        placeholder= "Noun"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="ptVerb2"
+                        onChange= { this.updateName }
+                        placeholder= "Past Tense Verb"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="ptVerb3"
+                        onChange= { this.updateName }
+                        placeholder= "Past Tense Verb"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="place"
+                        onChange= { this.updateName }
+                        placeholder= "Place"
+                        disabled = {this.state.disabled}
+                    ></input>
+                    <input
+                        name="verb"
+                        onChange= { this.updateName }
+                        placeholder= "Verb"
+                        disabled = {this.state.disabled}
+                    ></input>
+                </form>
+
+                <button onClick={ this.handleSubmit }>Submit</button>
+                <button onClick={ this.clearParagraph }>Clear</button>
+
+                { this.state.isTrue === true && <Paragraph words={this.state} /> }
             </div>
         );
     }
 }
+
+export default ListedInputs;
